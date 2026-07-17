f7ec5ee15e99c7e5c2eec6a7dd390502e942d77c
tanishq91-hub <tanishqmathur91@gmail.com>
2026-07-17 21:28:23 +0530

    feat: initialize user service with TypeScript, Express, and MongoDB integration
    - Add package.json with necessary dependencies and scripts
    - Create controller for user registration
    - Set up main application entry point with MongoDB connection
    - Define user registration route
    - Add TypeScript configuration for the project

---
+++ user service/src/controller.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express"
+
+export const registerUser = async (req: Request, res: Response) => {
+    res.json("Server is Running")
+}
---
+++ user service/src/index.ts
@@ -0,0 +1,38 @@
+import dns from "dns"
+import dotenv from "dotenv"
+import express from "express"
+import mongoose from "mongoose"
+import userRoutes from "./route.js"
+
+dotenv.config()
+
+dns.setServers(["1.1.1.1", "8.8.8.8"])
+
+const conncetDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI as string, {
+            dbName: "Spotify",
+        })
+
+        console.log("✅ MongoDB Connected")
+        console.log(`📦 Database: ${mongoose.connection.name}`)
+    } catch (error) {
+        console.log("❌ MongoDB Connection Failed")
+        console.log(error)
+    }
+}
+
+const app = express()
+
+app.use("/api/v1", userRoutes)
+
+app.get("/", (req, res) => {
+    res.send("Server is working")
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(5000, () => {
+    console.log(`Server is running on port ${port}`)
+    conncetDB()
+})
